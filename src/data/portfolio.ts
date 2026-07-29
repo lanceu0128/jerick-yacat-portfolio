2a8bcd2a3bfb5c83272fe35f5353773b4df7e661
Lance <lanceu0128@gmail.com>
2026-07-28 19:16:03 -0700

    video and writing sections

---
+++ src/data/portfolio.ts
@@ -0,0 +1,63 @@
+export interface PortfolioItem {
+  title: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+  href?: string;
+}
+
+const placeholderImage = "/temp-thumbnail.png";
+
+export const videoItems: PortfolioItem[] = [
+  {
+    title: "My Dollar",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod...",
+    imageSrc: placeholderImage,
+    imageAlt: "Placeholder thumbnail for My Dollar",
+    href: "https://www.youtube.com/watch?v=5-NMg_Ddx4I",
+  },
+  {
+    title: "My Dollar",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod...",
+    imageSrc: placeholderImage,
+    imageAlt: "Placeholder thumbnail for My Dollar",
+    href: "https://www.youtube.com/watch?v=5-NMg_Ddx4I",
+  },
+];
+
+export const writingItems: PortfolioItem[] = [
+  {
+    title: "My Dollar",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod...",
+    imageSrc: placeholderImage,
+    imageAlt: "Placeholder thumbnail for My Dollar",
+    href: "https://www.youtube.com/watch?v=5-NMg_Ddx4I",
+  },
+  {
+    title: "My Dollar",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod...",
+    imageSrc: placeholderImage,
+    imageAlt: "Placeholder thumbnail for My Dollar",
+    href: "https://www.youtube.com/watch?v=5-NMg_Ddx4I",
+  },
+  {
+    title: "My Dollar",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod...",
+    imageSrc: placeholderImage,
+    imageAlt: "Placeholder thumbnail for My Dollar",
+    href: "https://www.youtube.com/watch?v=5-NMg_Ddx4I",
+  },
+  {
+    title: "My Dollar",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod...",
+    imageSrc: placeholderImage,
+    imageAlt: "Placeholder thumbnail for My Dollar",
+    href: "https://www.youtube.com/watch?v=5-NMg_Ddx4I",
+  },
+];
